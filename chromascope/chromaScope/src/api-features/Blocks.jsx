50e21f223c5d6e138058da85c3ff1d53cd161a06
Kushagar Garg <dreamstick909@gmail.com>
2026-04-05 18:13:15 +0530

    added the sort button with no functionality yet also have a mock data of the api for testing purposes

--- chromascope/chromaScope/src/api-features/Blocks.jsx
+++ chromascope/chromaScope/src/api-features/Blocks.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const api = '/api/v1/blocks';
+import mockData from '../api.json';
+
+const mock = mockData;
 
 async function fetchBlockInfo(){
     try{
-        let k = await fetch(api);
-        let data = await k.json()
-        return (data)
+        // let k = await fetch(api);
+        // let data = await k.json()
+        return mock
     }catch(error){
         console.log(error)
         return []
--- chromascope/chromaScope/src/search-feature/Search.jsx
+++ chromascope/chromaScope/src/search-feature/Search.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import './search.css'
 function Search({setQuery}){
     return (
-        <>
+        <div className="outer">
         <label htmlFor="site-search">Search the site:</label>
+        <div className="inner">
             <input type="search" id="site-search" placeholder="Enter a pool name" onChange={(e)=>setQuery(e.target.value) }/>
+            <label htmlFor="sorting">Sort by:</label>
+            <select>
+                <option>Default</option>
+                <option>Total Fees</option>
+                <option>Size</option>
 
-        </>
+            </select>
+        </div>
+
+        </div>
     )
 }
 export default Search
